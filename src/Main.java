1a6d9036078c40f86274f444cfbe5b769b6656ac
MariaLaura01 <69858482+MariaLaura01@users.noreply.github.com>
2020-11-19 15:26:39 -0300

    Tarefa concluída
    OBS1.: Meu programa deu um erro de tempo, então tive que adcionar
    "useTimezone=true&serverTimezone=UTC" no getConnection.
    
    OBS2.: tive que escrever o comando do getConnection de uma forma
    diferente porque a que você pediu deu erro.
    
    Todos os erros foram corrigidos e o programa está rodando normal aqui.

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,26 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class Main {
-	public static void main(String[] args) {
+public class Main 
+{
+	public static void main(String[] args) 
+	{
 		
-		try {
+		try 
+		{
 			
 			// TODO Especifique a URL de conexão na linha abaixo
-			Connection conn = DriverManager.getConnection("");
-			
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/world?useTimezone=true&serverTimezone=UTC", "root", "abcD-123456S2");
+			//("jdbc:mysql://localhost:3306/world?useTimezone=true&serverTimezone=UTC" + "user=root&password=abcD-123456S2");
 			
 			System.out.println("Connected: " + conn.isValid(3));
-			// usaria a conexão aqui
+			
 			conn.close();
 			System.out.println("Connected: " + conn.isValid(3));
 			
-		} catch (SQLException e) {
+		} 
+		catch (SQLException e)
+		{
 			e.printStackTrace();
 		}
 	}
